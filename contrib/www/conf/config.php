9b8760c7f1bb018b46db260cac872e10fa8ebc83
Despite <despite.mn@gmail.com>
2013-04-09 00:39:10 +0300

    www reg init

---
+++ contrib/www/conf/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$auth = array(
+'db_host' => 'localhost',	// Host IP or Domain
+'db_user' => 'root',		// Auth user
+'db_pass' => 'bttsex123',	// Auth pass
+'db_name' => 'auth',		// Auth DB name. Usually either auth or realmd
+);
+
+?>
