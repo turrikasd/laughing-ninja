75b2be70ae6a73ea36613b88249c7ff21f1132ca
Appled <applezor1@hotmail.com>
2013-04-09 12:20:08 +0300

    Small fix to Karazhan

---
+++ sql/updates/2013-04-09-karazhan.sql
@@ -0,0 +1,6 @@
+-- As of patch 2.1, Nightbane's adds should not be immune to spell damage nor have immolation aura anymore
+DELETE FROM creature_template_spells WHERE entry='17261';
+-- Prince Malchezaar axes should be deadly, not only hit for 1. Damage is not blizzlike but gathered from many sources and is close to blizzlike. Also axes have 'Demonic Frenzy' passive aura.
+UPDATE creature_template SET mindmg='212', maxdmg='679', attackPower='100' WHERE (entry='17650');
+DELETE FROM creature_template_spells WHERE entry='17650';
+INSERT INTO creature_template_spells VALUES ('17650','32850','0','0','0');
